704d83c31e171e8ab87932b8bda20ebeba7b5f6f
Timothy Golio <TimothyVGolio@gmail.com>
2021-05-23 09:57:26 -0400

    Minor spacing changes

--- LAMPAPI/sign_up.php
+++ LAMPAPI/sign_up.php
@@ -23,19 +23,19 @@
   }
 
   function getRequestInfo()
-	{
+  {
 		return json_decode(file_get_contents('php://input'), true);
-	}
+  }
 
   function sendResultInfoAsJson( $obj )
-	{
+  {
 		header('Content-type: application/json');
 		echo $obj;
-	}
+  }
 
   function returnWithError( $err )
-	{
+  {
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
-	}
+  }
 ?>
