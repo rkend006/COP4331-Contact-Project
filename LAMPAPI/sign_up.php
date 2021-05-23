412fbfa68136525a5b78d4179b188beff6d389cd
Timothy Golio <TimothyVGolio@gmail.com>
2021-05-23 09:54:48 -0400

    Minor spacing changes

--- LAMPAPI/sign_up.php
+++ LAMPAPI/sign_up.php
@@ -3,23 +3,23 @@
   $inData = getRequestInfo();
 
   $firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
+  $lastName = $inData["lastName"];
   $login = $inData["login"];
   $password = $inData["password"];
 
   $conn = new mysqli("localhost", "Group15Admin", "ByVivec", "COP4331");
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
+  if( $conn->connect_error )
+  {
+    returnWithError( $conn->connect_error );
+  }
+  else
+  {
     $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
     $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
     $stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
   }
 
   function getRequestInfo()
