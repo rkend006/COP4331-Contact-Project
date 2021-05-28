d231009aa9e49f3c15247f0b1b1c87bd8d47a4ea
NicholasHabryl <84864635+NicholasHabryl@users.noreply.github.com>
2021-05-28 18:00:54 -0400

    Update for search_contacts.php

--- LAMPAPI/search_contacts.php
+++ LAMPAPI/search_contacts.php
@@ -12,10 +12,10 @@
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE FirstName LIKE '?'  AND LastName LIKE  '?'  AND UserID= ? ");
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE FirstName LIKE ?  AND LastName LIKE  ?  AND UserID= ? ");
 		$stmt->bind_param("ssi", $FirstName, $LastName, $inData["userID"]);
-		$FirstName = "%" . $inData["searchFirst"] . "%";
-		$LastName = "%" . $inData["searchLast"] . "%";
+		$FirstName = "'%" . $inData["searchFirst"] . "%'";
+		$LastName = "'%" . $inData["searchLast"] . "%'";
 		$stmt->execute();
 
 		if($result = $stmt->get_result())
