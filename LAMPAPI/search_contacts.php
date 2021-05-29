9642a58e4d642d856b9215cc4df53c2375b5eec8
Thomas <thomas@stoeckert.us>
2021-05-29 19:19:31 -0400

    Debugging Seach_contacts

--- LAMPAPI/search_contacts.php
+++ LAMPAPI/search_contacts.php
@@ -6,76 +6,64 @@
 	$searchCount = 0;
 
 	$conn = new mysqli("localhost", "Group15Admin", "ByVivec", "COP4331");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
+	if ($conn->connect_error) {
+		returnWithError($conn->connect_error);
 	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ?) AND (LastName LIKE ?) AND (UserID = ?) ");	
-		$FirstName = "'%" . $inData["searchFirst"] . "%'";
-		$LastName = "'%" . $inData["searchLast"] . "%'";
-		$stmt->bind_param("ssi", $FirstName, $LastName, $inData["userID"]);
 
-		//statement executes, returns either T or F
-		if($stmt->execute())
-		{
-			//$result should get the result set 
-			if($result = $stmt->get_result())
-			{}
-			else returnWithError("Debug: Empty result set");
-			
-			//in each iteraion of the loop, $row should get the next available row
-			while($row = $result->fetch_array())
-			{
-				if( $searchCount > 0 )
-				{
-					$searchResults .= ",";
-				}
-				$searchCount++;
-				$searchResults .= '"' . implode(",",$row) . '"';
-			}
+	$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ?) AND (LastName LIKE ?) AND (UserID = ?) ");
+	$FirstName = "'%" . $inData["searchFirst"] . "%'";
+	$LastName = "'%" . $inData["searchLast"] . "%'";
+	$stmt->bind_param("ssi", $FirstName, $LastName, $inData["userID"]);
+	$stmt->execute();
 
-			//incase of empty result set, return no results, otherwise return data
-			if( $searchCount == 0 )
-			{
-				returnWithError( "No results found.");
+	//$result should get the result set 
+	if ($result = $stmt->get_result()) {
+		echo "Got result";
+		while ($row = $result->fetch_array()) {
+			echo "Got row: ";
+			var_dump($row);
+
+			if ($searchCount > 0) {
+				$searchResults .= ",";
 			}
-			else
-			{
-				returnWithInfo( $searchResults );
-			}
+			$searchCount++;
+			$searchResults .= '"' . implode(",", $row) . '"';
 		}
 
-		else returnWithError( $stmt->error );
+		//incase of empty result set, return no results, otherwise return data
+		if ($searchCount == 0) {
+			returnWithError("No results found.");
+		} else {
+			returnWithInfo($searchResults);
+		}
+	} else returnWithError("Debug: Empty result set");
 
-		
+	//in each iteraion of the loop, $row should get the next available row
 
-		$stmt->close();
-		$conn->close();
-	}
+	$stmt->close();
+	$conn->close();
+
 
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 
-	function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"ID":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
 	}
 
-	function returnWithInfo( $searchResults )
+	function returnWithInfo($searchResults)
 	{
-		$retValue = '{"results": ['.$searchResults.'] ,"error":""}';
-		sendResultInfoAsJson( $retValue );
+		$retValue = '{"results": [' . $searchResults . '] ,"error":""}';
+		sendResultInfoAsJson($retValue);
 	}
-
 ?>
