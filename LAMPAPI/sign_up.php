1c3c2a35cefd3a66af0654737cf4f33215dd12bf
Timothy Golio <TimothyVGolio@gmail.com>
2021-05-29 20:08:10 -0400

    Changes sign_up so duplicate logins are not permitted

--- LAMPAPI/sign_up.php
+++ LAMPAPI/sign_up.php
@@ -14,27 +14,40 @@
   }
   else
   {
-    $stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-    $stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
-    $stmt->execute();
-    $lastID = $conn->insert_id;
-    $stmt->close();
-    $stmt2 = $conn->prepare("SELECT * FROM Users WHERE ID = ?");
-    $stmt2->bind_param("i", $lastID);
-    $stmt2->execute();
-    $result = $stmt2->get_result();
+    $stmt0 = $conn->prepare("SELECT * FROM Users WHERE Login = ?");
+    $stmt0->bind_param("s", $Login);
+    $stmt0->execute();
+    $result0 = $stmt0->get_result();
+    if( $row = $result0->fetch_assoc() )
+    {
+      returnWithError("User with the provided login already exists");
+      $stmt0->close();
+    }
+    else
+    {
+      $stmt0->close();
+      $stmt1 = $conn->prepare("INSERT into Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+      $stmt1->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
+      $stmt1->execute();
+      $lastID = $conn->insert_id;
+      $stmt1->close();
+      $stmt2 = $conn->prepare("SELECT * FROM Users WHERE ID = ?");
+      $stmt2->bind_param("i", $lastID);
+      $stmt2->execute();
+      $result = $stmt2->get_result();
 
-    if( $row = $result->fetch_assoc() )
-		{
-			returnWithInfo( $row['ID'], $row['FirstName'], $row['LastName'], $row['Login'] );
-		}
-		else
-		{
-			returnWithError("Bad Input Syntax");
-		}
+      if( $row = $result->fetch_assoc() )
+      {
+        returnWithInfo( $row['ID'], $row['FirstName'], $row['LastName'], $row['Login'] );
+      }
+      else
+      {
+        returnWithError("Bad Input Syntax");
+      }
 
-    $stmt2->close();
-    $conn->close();
+      $stmt2->close();
+      $conn->close();
+    }
   }
 
   function getRequestInfo()
